eb4c757ad341148ce0fbb04681d435bd79cb5dd1
Jacob Forte <jforte7@kent.edu>
2019-03-04 22:14:58 -0500

    Update tblUserClassComment.sql

--- SQL/TableCreateScripts/tblUserClassComment.sql
+++ SQL/TableCreateScripts/tblUserClassComment.sql
@@ -8,5 +8,5 @@ CREATE TABLE tblUserClassComment (
 	rating integer NOT NULL,
 	PRIMARY KEY(email, classCRN),
 	FOREIGN KEY(email) REFERENCES tblUsers(email),
-	FOREIGN KEY(classCRN) REFERENCES tblBooks(crn)
+	FOREIGN KEY(classCRN) REFERENCES tblClasses(crn)
 );
