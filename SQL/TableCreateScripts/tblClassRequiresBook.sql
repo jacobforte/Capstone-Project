6d902ad79554e395e1ea49391685f50d08fae854
Jacob Forte <jforte7@kent.edu>
2019-03-04 21:58:47 -0500

    Update tblClassRequiresBook.sql

--- SQL/TableCreateScripts/tblClassRequiresBook.sql
+++ SQL/TableCreateScripts/tblClassRequiresBook.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS tblClassRequiresBook;
 
 CREATE TABLE tblClassRequiresBook (
-	classCRN varchar(255),
-	bookISBN varchar(255),
+	classCRN varchar(255) NOT NULL,
+	bookISBN varchar(255) NOT NULL,
 	isRequired boolean,
 	PRIMARY KEY(classCRN, bookISBN),
 	FOREIGN KEY (classCRN) REFERENCES tblClasses(crn),
