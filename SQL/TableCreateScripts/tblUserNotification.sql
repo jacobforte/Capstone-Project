331fe60391ea2496e21744ce7bbc9b5c380bd3ff
Jacob Forte <jforte7@kent.edu>
2019-03-04 22:07:00 -0500

    Update tblUserNotification.sql

--- SQL/TableCreateScripts/tblUserNotification.sql
+++ SQL/TableCreateScripts/tblUserNotification.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS tblUserNotification;
 CREATE TABLE tblUserNotification (
 	email varchar(255) NOT NULL,
 	notificationType varchar(255) NOT NULL,
-	PRIMARY KEY(email, notificationType)
+	PRIMARY KEY(email, notificationType),
 	FOREIGN KEY(email) REFERENCES tblUsers(email),
 	FOREIGN KEY(notificationType) REFERENCES tblNotifications(type)
 );
