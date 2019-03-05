192a0af67f316f235c2b37154f94351e45d6dbcd
Jacob Forte <jforte7@kent.edu>
2019-03-05 11:50:46 -0500

    Update tblUserClassComment.sql

--- SQL/TableCreateScripts/tblUserClassComment.sql
+++ SQL/TableCreateScripts/tblUserClassComment.sql
@@ -4,8 +4,11 @@ CREATE TABLE tblUserClassComment (
 	email varchar(255) NOT NULL,
 	classCRN varchar(255) NOT NULL,
 	shortDescription varchar(255),
-	longDescription varchar(2550),
 	rating integer NOT NULL,
+	reviewDate date,
+	instructor varchar(255),
+	semester varchar(255),
+	campus varchar(255),
 	PRIMARY KEY(email, classCRN),
 	FOREIGN KEY(email) REFERENCES tblUsers(email),
 	FOREIGN KEY(classCRN) REFERENCES tblClasses(crn)
