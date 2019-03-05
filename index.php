f5f6f10c53b39da637ec1243c90f47f71e8e9a7e
Jacob Forte <jforte7@kent.edu>
2019-03-05 12:28:12 -0500

    seperated the class into its own file

--- index.php
+++ index.php
@@ -1,46 +1,10 @@
 <?php
+require_once('someClass.inc.php');
 /**
  * \file
  * A documented file.
- * This file contains test documentation, look over it to see examples of doxygen
+ * This file contains test documentation for a web page, look over it to see examples of doxygen
  */
-
-/**
- * A test class. This is a brief description, the brief ends at the first period or first endline. See?\n
- * This class is used for testing doxygen.\n
- * This is an in-depth description for this class\n
- * It can store and return a variable of any type\n
- * Doxygen requires \\n in orderr to end the line.
- * 
- * Adding a line space adds a larger gap between lines.
- */
-class Test {
-  public $htmlOut; /**< This is a public variable. The less than symbol in the code is needed for comments after a line of code. */
-  private $invisibleVar; /**< This variable wont show in the documentation because it is private */
-
-  /**
-   * A set function.
-   * INPUT: Any variable
-   * OUTPUT: None
-   * 
-   * This function sets the htmlOut variable
-   */
-  public function setHtmlOut($string) {
-    $this->htmlOut = $string;
-  }
-
-  /**
-   * A get function.
-   * INPUT: None
-   * OUTPUT: A string that has been previously set.
-   * 
-   * This functions returns the variable that was previously set.
-   * This line of text was updated by the server automaticly.
-   */
-  public function getHtmlOut() {
-    return $this->htmlOut;
-  }
-}
 
 /**
  * The test output function returns static text.
@@ -49,7 +13,8 @@ class Test {
  * INPUT: none
  * OUTPUT: a string containing an html page
  */
-public function TestOutput() {
+
+function TestOutput() {
   return "<h1>Hello World</h1>
       <p>Written by Jacob Forte</p>
       <p>Some change 4</p>
