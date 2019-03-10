42921fe7ece25b95fe857b9947d50b01d6e487d4
Jacob Forte <jforte7@kent.edu>
2019-03-10 19:36:16 -0400

    more testing

--- SQL/StoredProcedures/spDeleteUser.sql
+++ SQL/StoredProcedures/spDeleteUser.sql
@@ -1,11 +1,3 @@
-/**
- * \file spDeleteUser.sql
- * Deletes a user from a database.
- * This also deletes all UserNotifications, UserRegisteredClasses, posted books (along with the list of photos), and comments.
- * The actual photo will need to be removed./n
- * IN email varchar(255)
- * OUT none
-*/
 DELIMITER $$
 DROP PROCEDURE IF EXISTS spDeleteUser$$
 
