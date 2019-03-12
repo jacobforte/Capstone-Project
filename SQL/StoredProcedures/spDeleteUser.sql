e72be6d67368d8e575d78820c317daf025ed63e5
Jacob Forte <jforte7@kent.edu>
2019-03-12 19:52:54 -0400

    Update spDeleteUser.sql

--- SQL/StoredProcedures/spDeleteUser.sql
+++ SQL/StoredProcedures/spDeleteUser.sql
@@ -3,19 +3,23 @@ DROP PROCEDURE IF EXISTS spDeleteUser$$
 
 CREATE PROCEDURE spDeleteUser
 (
-    IN useremail varchar(255)
+    IN email varchar(255)
 )
 BEGIN
     START TRANSACTION;
-    DELETE FROM tblUserNotifications WHERE email = useremail;
-    DELETE FROM tblUserRegisteredClasses WHERE email = useremail;
-    DELETE FROM tblUserClassComment WHERE email = useremail;
+    DELETE FROM tblUserNotifications WHERE email = email;
+    DELETE FROM tblUserRegisteredClasses WHERE email = email;
+    DELETE FROM tblUserClassComment WHERE email = email;
 
     DELETE FROM tblUserSellBookPhoto
-    LEFT OUTER JOIN tblUserSellBook ON tblUserSellBookPhoto.id = tblUserSellBook.id AND tblUserSellBook.email = useremail;
+    WHERE id IN 
+    (
+        SELECT id FROM tblUserSellBook b
+        WHERE b.email = email
+    )
 
-    DELETE FROM tblUserSellBook WHERE email = useremail;
-    DELETE FROM tblUsers WHERE email = useremail;
+    DELETE FROM tblUserSellBook WHERE email = email;
+    DELETE FROM tblUsers WHERE email = email;
     COMMIT TRANSACTION;
 END$$
 DELIMITER ;
