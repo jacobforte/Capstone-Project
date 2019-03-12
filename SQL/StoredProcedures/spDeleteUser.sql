9c371fe120111fb6df0ae0a86b8cdca531a5a427
Jacob Forte <jforte7@kent.edu>
2019-03-12 15:44:59 -0400

    Update spDeleteUser.sql

--- SQL/StoredProcedures/spDeleteUser.sql
+++ SQL/StoredProcedures/spDeleteUser.sql
@@ -14,9 +14,9 @@ BEGIN
     DELETE FROM tblUserSellBookPhoto p
     WHERE (p.id) IN
         (
-            SELECT p.id
-            FROM tblUserSellBook
-            Where p.email = email
+            SELECT b.id
+            FROM tblUserSellBook b
+            Where b.email = email
         );
 
     DELETE FROM tblUserSellBook WHERE email = email;
