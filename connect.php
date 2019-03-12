0cd91ef0ae173eac55e75a09d8dbfea838b45132
Time to upgrade? <vta@kent.edu>
2019-03-12 17:21:23 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,10 @@
-<html>
 <!DOCTYPE html>
-<html>
-<head>
-     <meta http-equiv='content-type' content='text/html;charset=utf-8' />
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
      <title>Capstone Project: BookIt </title>
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 </head>
