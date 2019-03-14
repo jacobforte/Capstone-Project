08dea25765cdff72195ff52630d0bce781e67b7d
Jacob Forte <jforte7@kent.edu>
2019-03-14 09:39:45 -0400

    Revert "Update spDeleteUser.sql"
    This reverts commit 6ebac937cbd3b666623c0d635b7ca78531a83f32.

--- SQL/StoredProcedures/spDeleteUser.sql
+++ SQL/StoredProcedures/spDeleteUser.sql
@@ -7,21 +7,19 @@ CREATE PROCEDURE spDeleteUser
 )
 BEGIN
     START TRANSACTION;
-    SET @email = email;
-    
-    DELETE FROM tblUserNotification WHERE email = @email;
-    DELETE FROM tblUserRegisteredClasses WHERE email = @email;
-    DELETE FROM tblUserClassComment WHERE email = @email;
+    DELETE FROM tblUserNotification WHERE tblUserNotification.email = email;
+    DELETE FROM tblUserRegisteredClasses WHERE tblUserRegisteredClasses.email = email;
+    DELETE FROM tblUserClassComment WHERE tblUserClassComment.email = email;
 
     DELETE FROM tblUserSellBookPhoto
     WHERE tblUserSellBookPhoto.id IN 
     (
         SELECT tblUserSellBook.id FROM tblUserSellBook
-        WHERE tblUserSellBook.email = @email
+        WHERE tblUserSellBook.email = email
     );
 
-    DELETE FROM tblUserSellBook WHERE email = @email;
-    DELETE FROM tblUsers WHERE email = @email;
+    DELETE FROM tblUserSellBook WHERE tblUserSellBook.email = email;
+    DELETE FROM tblUsers WHERE tblUsers.email = email;
     COMMIT;
 END$$
 DELIMITER ;
