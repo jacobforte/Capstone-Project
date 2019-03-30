27f6b4a2ecb3e024bd485c8e34d0df56f2b2d0e9
Jacob Forte <jforte7@kent.edu>
2019-03-30 14:51:43 -0400

    Update searchResults.function.php

--- resources/functions/searchResults.function.php
+++ resources/functions/searchResults.function.php
@@ -2,8 +2,7 @@
 require_once("resources/functions/dbconnection.function.php");
 
 function outputClasses($search) {
-    $classes = array();
-    //$classes = dbconnection("spSelectClasses(NULL, {$search}, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)");
+    $classes = dbconnection("spSelectClasses(NULL, {$search}, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)");
 
     if (count($classes) == 0) {
         echo "<div class='row mt-2'>
