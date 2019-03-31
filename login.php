aa1d6927804d1e8290bc85d045c8309e9006bc73
mjohn113 <mjohn113@kent.edu>
2019-03-31 02:06:31 -0400

    Update login.php

--- login.php
+++ login.php
@@ -7,6 +7,36 @@
         
         <main>
             
+            <div  class="container-fluid" style="width: 500px; height: 800px;">
+
+<div class="row">
+
+<div class="col-sm-12 text-center" >
+
+  <div class="panel panel-default ">
+
+	<div id="loginform" class="panel-body">
+	<h1>Sign-in</h1>
+		<form action="userlogin.php" method="post">
+			<div class="form-group text-left">
+				<label for="email">Email address:</label>
+				<input type="email" class="form-control" name="email">
+			</div>
+			<div class="form-group text-left">
+				<label for="pwd">Password:</label>
+				<input type="password" class="form-control" name="pwd">
+			</div>
+				<button type="submit" class="btn btn-warning btn-block">Submit</button>
+				<br></br>
+			<a href="register.php"><p>Don't have an Account?</p></a>
+		</form>
+	</div>
+  </div>
+  
+ </div>
+</div> 
+</div>
+            
         </main>
 
         <?php include("resources/includes/footer.inc.php"); ?>
