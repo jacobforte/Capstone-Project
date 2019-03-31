ae2a01c779f04aa5c5824d6f57043769d498e7ce
mjohn113 <mjohn113@kent.edu>
2019-03-30 22:48:29 -0400

    Update post.php

--- post.php
+++ post.php
@@ -1,13 +1,6 @@
 <?php
 
-	require_once('config.php');
-
-	$sql = "INSERT INTO tblUsers (email, name, password)
-	VALUE ('$_POST[email]', '$_POST[dnam]', '$_POST[pwd]')";
-	
-	mysqli_query($connect, $sql);
-	
-	
+dbconnection($spNewUser($_POST['email'], $_POST['dnam'], $_POST['pwd']));
 	
 	
 	header('Location: login.php');
