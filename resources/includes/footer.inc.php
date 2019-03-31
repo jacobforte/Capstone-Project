61f949d143fd15817a19d3e3079bab03edb49adc
mjohn113 <mjohn113@kent.edu>
2019-03-30 22:13:40 -0400

    Update footer.inc.php

---
+++ resources/includes/footer.inc.php
@@ -0,0 +1,36 @@
+<footer>
+
+
+
+<div id="navi" class="container-fluid">
+	
+	<div class="col-sm-4">
+	<div class="row">
+	<h1>Book-it</h1>
+	</div>
+	
+	<p id="storloc">Kent state</p>
+	
+	</div>
+	
+	<div id="foottext" class="col-sm-4 text-center">
+	
+		<a href="#navi"><p>Back to top of page.</p></a>
+		<span class="glyphicon glyphicon-arrow-up" style="color: rgb(239, 171, 0);"></span>
+
+	
+	</div>
+	
+	<div id="foottext" class="col-sm-4 text-right">
+		<p>Kent State Stark - Capstone Project</p>
+		<p>Created by Jacob, Marcus, Vina, and Zach</p>
+	</div>
+	
+</div>
+
+
+
+
+
+
+</footer>
