f6c03356206ab85da532379625b5d0ef80b783a9
Time to upgrade? <vta@kent.edu>
2019-04-02 16:16:11 -0400

    Update bookListings.php

--- bookListings.php
+++ bookListings.php
@@ -1,7 +1,21 @@
 <!doctype html>
 <html lang="en">
-
     <head>
+        <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv='content-type' content='text/html;charset=utf-8' />
+
+<link rel="stylesheet" href="resources/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="resources/js/bootstrap.min.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700" rel="stylesheet">
+
+<link rel="stylesheet" href="resources/css/capstoneCSSFile.css">    </head>
+
         <?php include("resources/includes/head.inc.php"); ?>
     </head>
     <body>
@@ -16,7 +30,7 @@
 	<h1> Title <?php $row['title']?> </h1>
 		<p> <b> Author </b> <br><?php $row['author']?> </br> </p>
 		<p> <b> ISBN </b> <br> <?php $row['isbn']?> </br> </p>
-		<button type="button" a href="postABook.php"> Sell Your Textbook </button>
+		<button type="button" class="btn btn-warning" action="postABook.php"> Sell Your Textbook </button>
 	<div class="row">
     <div class="col-sm-auto mt-3">
 	<h2> Filters </h2>
@@ -45,7 +59,7 @@
 			      </div>
 			    </form>
 			  </div>
-		<input type="submit" data-inline="true" value="Submit">
+		<input type="submit" data-inline="true" class="btn btn-warning" value="Submit">
 	</div>
 
 	</main>
