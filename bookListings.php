dca47383ebdfe114e48d23e09a72dc67d148618b
Time to upgrade? <vta@kent.edu>
2019-04-02 16:13:44 -0400

    Update bookListings.php

--- bookListings.php
+++ bookListings.php
@@ -1,9 +1,5 @@
 <!doctype html>
 <html lang="en">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
-<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> 
 
     <head>
         <?php include("resources/includes/head.inc.php"); ?>
