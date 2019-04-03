2109fc055ebae0f935b3342ad5ae6835543926c4
mjohn113 <mjohn113@kent.edu>
2019-04-03 12:55:53 -0400

    now matches design of login.php

--- register.php
+++ register.php
@@ -8,43 +8,44 @@
 
         <main>
 		<br></br>
-            <div  class="container-fluid" style="width: 500px; height: 800px;">
+        <div  class="container-fluid" >
 
-<div class="row">
 
-<div class="col-sm-12 text-center" >
+			<div class="row">
 
-  <div class="panel panel-default ">
+			<div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+                <div class="card bg-blue mt-5">
+                    <div class="card-body px-4 text-white">
+                        <h4 class="font-weight-bold text-center">Create Account</h4>
+                        <form action="post.php" method="post">
+							<div class="form-group text-left">
+								<label for="email">Email address:</label>
+								<input type="email" class="form-control" name="email">
+							</div>
+							<div class="form-group text-left">
+								<label for="email">Display Name:</label>
+								<input type="text" class="form-control" name="dnam">
+							</div>
+							<div class="form-group text-left">
+								<label for="pwd">Password:</label>
+								<input type="password" class="form-control" name="pwd">
+							</div>
+							<div class="form-group text-left">
+								<label for="pwd">Confirm Password:</label>
+								<input type="password" class="form-control" name="pwd">
+							</div>
+								<button type="submit" class="btn btn-warning btn-block">Submit</button>
+								<br></br>
+								<a href="login.php" class="text-white text-center"><p>Already have an Account?</p></a>
+						</form>
+                    </div>
+                </div>
+            </div>
 
-	<div id="loginform" class="panel-body">
-	<h1>Sign-in</h1>
-		<form action="post.php" method="post">
-			<div class="form-group text-left">
-				<label for="email">Email address:</label>
-				<input type="email" class="form-control" name="email">
-			</div>
-			<div class="form-group text-left">
-				<label for="email">Display Name:</label>
-				<input type="text" class="form-control" name="dnam">
-			</div>
-			<div class="form-group text-left">
-				<label for="pwd">Password:</label>
-				<input type="password" class="form-control" name="pwd">
-			</div>
-			<div class="form-group text-left">
-				<label for="pwd">Confirm Password:</label>
-				<input type="password" class="form-control" name="pwd">
-			</div>
-				<button type="submit" class="btn btn-warning btn-block">Submit</button>
-				<br></br>
-			<a id="foot" href="login.php"><p>Already have an Account?</p></a>
-		</form>
-	</div>
-  </div>
-  
- </div>
-</div> 
-</div>
+
+		</div>
+		</div> 
+
 
         </main>
 
