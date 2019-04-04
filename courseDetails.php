6249225184a3ac5fc9c2649b8b3cf73a000a02cd
Jacob Forte <jforte7@kent.edu>
2019-04-04 18:42:47 -0400

    Trying to fix documentation.

--- courseDetails.php
+++ courseDetails.php
@@ -1,4 +1,4 @@
-<?php include('resources/functions/course/course.details.list.function.php'); ?>
+<?php //include('resources/functions/course/course.details.list.function.php'); ?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -148,9 +148,9 @@
 
     </body>
 
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+    <!--<script src="https://code.jquery.com/jquery-2.2.4.min.js"
             integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
             crossorigin="anonymous"></script>
 
-    <script src="resources/js/course.details.js"></script>
+    <script src="resources/js/course.details.js"></script>-->
 </html>
