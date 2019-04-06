93438b5b01ea1df59d9f4e87c048507f630fee1e
Jacob Forte <jforte7@kent.edu>
2019-04-06 13:37:22 -0400

    Update classResults.class.php

--- resources/functions/searchResults/classResults.class.php
+++ resources/functions/searchResults/classResults.class.php
@@ -40,8 +40,8 @@
                 echo "There are no results for \"{$this->searchString}\"<br>";
             }
             else {
+                echo "<strong>Showing ". count($this->singleClassResultArray) ." for \"{$this->searchString}\"</strong><br>";
                 foreach ($this->singleClassResultArray as $row) {
-                    echo "<strong>Showing ". count($this->singleClassResultArray) ." for \"{$this->searchString}\"</strong><br>";
                     $row->print();
                 }
             }
