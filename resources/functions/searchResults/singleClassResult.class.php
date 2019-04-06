87b660b1a6969428e8bf4609766ca3eb9b84baf9
Jacob Forte <jforte7@kent.edu>
2019-04-06 12:44:58 -0400

    Update singleClassResult.class.php

--- resources/functions/searchResults/singleClassResult.class.php
+++ resources/functions/searchResults/singleClassResult.class.php
@@ -14,7 +14,7 @@
         public function __construct($dbArrayRow) {
             $this->courseID = preg_replace("/-[0-9]\*$/", "" ,$dbArrayRow["courseID"]);
             $this->title = $dbArrayRow["title"];
-            $this->lastUpdated = timeSinceDate(date("Y-m-d h:i:s", $dbArrayRow["lastUpdated"]));
+            $this->lastUpdated = date("Y-m-d h:i:s", $dbArrayRow["lastUpdated"]);
             $this->seatsRemaining = $dbArrayRow["seatsRemaining"];
             $this->campus = array();
             $this->campus[] = $dbArrayRow["campus"];
@@ -47,7 +47,7 @@
         public function getSeatsRemaining() {return $this->seatsRemaining;}
         public function getCampus() {return $this->campus;}
 
-        public function timeSinceDate($time)
+        private function timeSinceDate($time)
         {
             $time = time() - $time;
             $time = ($time<1)? 1 : $time;
