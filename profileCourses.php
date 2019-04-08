c3ed33ea2f386576262dc1f177966fbb64eefe32
zbrockway <zbrockwa@kent.edu>
2019-04-08 13:09:55 -0400

    Add links to profile sidebar

--- profileCourses.php
+++ profileCourses.php
@@ -7,7 +7,18 @@
         <?php include("resources/includes/header.inc.php"); ?>
 
         <main>
-
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-2 col-12 py-2 py-lg-3 pb-lg-4 bg-light">
+                        <nav class="nav nav-pills flex-column flex-sm-row flex-lg-column">
+                            <a class="h5 mb-1 text-blue font-weight-bold flex-fill text-lg-right text-center nav-link" href="profileAccount.php">Account</a>
+                            <a class="h5 mb-1 text-blue font-weight-bold flex-fill text-lg-right text-center nav-link" href="profileNotifications.php">Notifications</a>
+                            <a class="h5 mb-1 text-blue font-weight-bold flex-fill text-lg-right text-center nav-link" href="profileCourses.php">Courses</a>
+                            <a class="h5 mb-1 text-blue font-weight-bold flex-fill text-lg-right text-center nav-link" href="profileListings.php">Listings</a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
         </main>
 
         <?php include("resources/includes/footer.inc.php"); ?>
