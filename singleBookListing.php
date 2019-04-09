4b975cfd278989d43919be32e0441a9ae8409e53
Time to upgrade? <vta@kent.edu>
2019-04-09 11:52:22 -0400

    Update singleBookListing.php

--- singleBookListing.php
+++ singleBookListing.php
@@ -32,7 +32,7 @@ $books = dbconnection("spSelectUserSellBook( \"". $isbn. "\", \"". $title. "\", 
 										echo $row['isbn'];
 									 }
 										?> </b> <br>  </br> </p>
-							<button type="button" class="btn btn-warning" action="postABook.php"> Sell Your Textbook </button>
+	    <ahref class="btn btn-warning" action="postABook.php"> Sell Your Textbook </a>
 					</div>
 				</div>
 			</div>
