1fbbf671922b2ac91580328ec1c86b763eb479c8
Jacob Forte <jforte7@kent.edu>
2019-04-11 14:48:38 -0400

    Initial build for Single Book Listing

--- singleBookListing.php
+++ singleBookListing.php
@@ -1,51 +1,65 @@
 <?php 
-require_once("resources/functions/dbconnection.function.php");
-$isbn = "12345";
-$title ="Test";
-$author = "Thanos";
-$books = dbconnection("spSelectUserSellBook( \"". $isbn. "\", \"". $title. "\", \"". $author. "\", NULL,NULL, NULL)");
+    require_once("resources/functions/dbconnection.function.php");
+
+    $bookID = "";
+    if (isset($_GET["id"])) {
+        $bookID = $_GET["id"];
+    }
+
+    $bookData = dbconnection("spSelectUserSellBook({$bookID}, NULL, NULL, NULL, NULL, NULL)")[0];
+    $photos = dbconnection("spSelectUserSellBookPhoto({$bookID}");
 ?>
 <!doctype html>
 <html lang="en">
-<head>
-  <title>Book Listing</title>
-  <?php include("resources/includes/head.inc.php"); ?>
-</head> 
+    <head>
+        <title>Book Listing</title>
+        <?php include("resources/includes/head.inc.php"); ?>
+        <link type="text/css" rel="stylesheet" href="resources/css/lightslider.css">
+        <script src="resources/js/lightslider.js"></script>
+    </head> 
     <body>
         <?php include("resources/includes/header.inc.php"); ?>
-       <main>
-			<div class="col">
-				<div class="container">
-					<div class="col-md-auto mt-9">
-						<h1> Title <?php 
-									foreach ($books as $book) {
-										echo $row['title'];
-									 }
-										?> </h1>
-							<p> <b> Author  <?php 
-									foreach ($books as $book) {
-										echo $row['author'];
-									 }
-										?> </b> <br></br> </p>
-							<p> <b> ISBN  <?php 
-									foreach ($books as $book) {
-										echo $row['isbn'];
-									 }
-										?> </b> <br>  </br> </p>
-	    <a class="btn btn-warning" action="postABook.php"> Sell Your Textbook </a>
-					</div>
-				</div>
-			</div>
-				<h2> <?php 
-									foreach ($books as $book) {
-										echo $row['title'];
-									 }
-					?> Images </h2>
-	
 
-		</main>
-		
-    <?php include("resources/includes/footer.inc.php"); ?>
+        <main class="container my-4">
+            <div class="row">
+                <div class="col">
+                    <h1 class="mt-2"><?php echo $bookData["title"];?></h1>
+                    <h2 class="mt-2">Seller</h2>
+                    <p><?php echo $bookData["email"];?></p>
+                    <h2 class="mt-2">ISBN</h2>
+                    <p><?php echo $bookData["bookISBN"];?></p>
+                    <h2 class="mt-2">Condition</h2>
+                    <p><?php echo $bookData["bookCondition"];?></p>
+                    <h2 class="mt-2">Price</h2>
+                    <p>$<?php
+                            if ($bookData["price"] = "") {echo "0.00";}
+                            else {echo $bookData["price"];}
+                        ?></p>
+                    <h2 class="mt-2">Listing Description</h2>
+                    <p><?php echo $bookData["longDescription"];?></p><br>
+                    <button class="btn bg-orange mt-4 mb-4" type="button">Contact Seller</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h1>Pictures</h1>
+                    <ul id="lightSlider">
+                        <li>
+                            <h3>First Slide</h3>
+                            <p>Lorem ipsum Cupidatat quis pariatur anim.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+
+        <?php include("resources/includes/footer.inc.php"); ?>
 
     </body>
 </html>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $("#lightSlider").lightSlider(); 
+  });
+</script>
