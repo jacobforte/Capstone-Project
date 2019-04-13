220637df572b6229214f7f39db02caae8edd933b
mjohn113 <mjohn113@kent.edu>
2019-04-13 18:30:26 -0400

    basic stuff for now still need to add more

--- profileCourses.php
+++ profileCourses.php
@@ -17,6 +17,53 @@
                             <a class="h5 mb-1 text-blue font-weight-bold flex-fill text-lg-right text-center nav-link" href="profileListings.php">Listings</a>
                         </nav>
                     </div>
+					<div class="col-12 col-lg-10 pt-4 pb-4">
+						<h1>Courses</h1>
+						<br>
+						<h2>You can update your notifications for your registered course by selecting the notifications tab on the left.</h2>
+						<br>
+						<?php
+							include_once("resources/functions/dbconnection.function.php");
+						
+							$user = $_SESSION['user']['email'];
+							
+							$result = dbconnection("spSelectUserRegisteredClasses('$user')");
+							
+							foreach($result as $row){
+								
+								echo '<a href="#"><h3><strong>' . $row['title'] . '</strong></h3></a>';		
+								
+							echo '<div class="container-fluid row">';
+							echo '<div class="col-2" style="padding-left: 0;">';
+								
+
+								
+								echo '<h3>CRN: ' . $row['crn'] . '</h3>';
+								echo '<h3>Campus: ' . $row['campus'] . '</h3>';
+								echo '<h3>Credits: ' . $row['credits'] . '</h3>';
+								echo '<form action="#">';
+								echo '<button type="submit" class="btn btn-lg bg-orange w-160p"><strong>Book Info</strong></button>';
+								echo '</form>';
+								
+							echo '<br></br></div>';
+
+							echo '<div class="col-10">';
+							
+								echo '<h3>Start Date: ' . $row['startDate'] . ' End Date: ' . $row['endDate'] . '</h3>';
+								echo '<h3>Meeting Days: ' . $row['meetDays'] . ' Time: ' . $row['startTime'] . '-' . $row['endTime'] . '</h3>';
+								echo '<h3>Instructor: ' . $row['instructor'] . '</h3>';
+								echo '<form action="#">';
+								echo '<button type="submit" class="btn btn-lg bg-orange w-160p"><strong>Remove</strong></button>';
+								echo '</form>';
+								
+							echo '</div><br>';
+							
+							}
+							
+							
+							
+						?>
+					</div>
                 </div>
             </div>
         </main>
