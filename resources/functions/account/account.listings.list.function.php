d336a42ecfeccc0af132babfb4381ffb612147b7
zbrockway <zbrockwa@kent.edu>
2019-04-13 00:28:20 -0400

    Should probably have the .php extension

--- resources/functions/account/account.listings.list.function.php
+++ resources/functions/account/account.listings.list.function.php
@@ -24,7 +24,7 @@ function outputUserListings($user) {
         echo '<div class="mb-3" id="' . $listing["id"] . '">
             <div class="row">
                 <div class="col-12">
-                    <h5 class="font-weight-bold"><a href="singleBookListing?id=' . $listing['id'] .'">' . $listing['title'] . '</a></h5>
+                    <h5 class="font-weight-bold"><a href="singleBookListing.php?id=' . $listing['id'] .'">' . $listing['title'] . '</a></h5>
                 </div>
             </div>
             <div class="row">
