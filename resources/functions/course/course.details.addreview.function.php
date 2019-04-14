a49acbe59e1eddb49333526c75e44a78aa41d184
zbrockway <zbrockwa@kent.edu>
2019-04-14 18:47:16 -0400

    Try to fix weird Doxygen output

--- resources/functions/course/course.details.addreview.function.php
+++ resources/functions/course/course.details.addreview.function.php
@@ -2,18 +2,6 @@
 
 require("../dbconnection.function.php");
 
-if(isset($_POST['email']) && isset($_POST['crn']) && isset($_POST['review']) && isset($_POST['rating'])
-    && isset($_POST['instructor']) && isset($_POST['semester']) && isset($_POST['campus'])) {
-    addReviewFor($_POST["email"], $_POST["crn"], $_POST["review"], $_POST["rating"], $_POST["instructor"], $_POST["semester"], $_POST["campus"]);
-}
-else {
-    $response_array['status'] = 'error';
-    $response_array['error'] = 'Please enter all fields.';
-    header('Content-type: application/json');
-    echo json_encode($response_array);
-}
-
-
 /** \file */
 /**
  * This function is used to add a review for a specific course.
@@ -27,4 +15,15 @@ else {
  */
 function addReviewFor($email, $crn, $review, $rating, $instructor, $semester, $campus) {
     dbconnection("spNewUserClassComment(\"" . $email . "\", \"" . $crn . "\", \"" . $review . "\", \"" . $rating . "\", null, \"" . $instructor . "\", \"" . $semester . "\", \"" . $campus . "\")");
+}
+
+if(isset($_POST['email']) && isset($_POST['crn']) && isset($_POST['review']) && isset($_POST['rating'])
+    && isset($_POST['instructor']) && isset($_POST['semester']) && isset($_POST['campus'])) {
+    addReviewFor($_POST["email"], $_POST["crn"], $_POST["review"], $_POST["rating"], $_POST["instructor"], $_POST["semester"], $_POST["campus"]);
+}
+else {
+    $response_array['status'] = 'error';
+    $response_array['error'] = 'Please enter all fields.';
+    header('Content-type: application/json');
+    echo json_encode($response_array);
 }
