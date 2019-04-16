67a6755c64cf273e6094476411d197b931ceb02c
Time to upgrade? <vta@kent.edu>
2019-04-16 17:14:34 -0400

    Update bookListings.php

--- bookListings.php
+++ bookListings.php
@@ -24,7 +24,7 @@
 			<div class="col">
 				<div class="container">
 					<div class="col-md-auto mt-9">
-                        
+
                         <h1> <?php echo $book["title"];?> </h1>
                         <p> 
                             <strong>
@@ -46,8 +46,40 @@
                         </p>
                         <button class="btn btn-warning" onclick="window.location.href='/postABook.php'">Post a Book</button>
                         </div>
+                        <form action='<?php echo $_POST['UserSellBook']; ?>' method='post' name='form_filter' >
+
+                <select name="value">
+                    <option value="all">All</option>
+                    <option value="price-asc-rank">Price: Low to High</option>     
+                    <option value="price-desc-rank">Price: High to Low</option>
+
+                                                    
+                </select>
+
+                <br />
+
+                <input type='submit' value = 'Filter'>
+
+                </form>
+                <?php
+
+                //connect to database, checking, etc
+
+                if($_POST['value'] == 'price'){
+                // query to get price 
+                $query = "SELECT * FROM price WHERE name='price'";
+                } else {
+                // query to get all price
+                $query = "SELECT * FROM price";
+                }
+                $sql = mysql_query($query);
+
+                while ($row = mysql_fetch_array($query)){
+                    $price = $row["price"];       
+                ?>
                         </div>
                     </div>
+
         </div>
     </main>
 
