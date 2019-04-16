fab37fc9cef30f416005c06603b39afc49da0629
Time to upgrade? <vta@kent.edu>
2019-04-16 14:38:30 -0400

    Update bookListings.php

--- bookListings.php
+++ bookListings.php
@@ -6,8 +6,8 @@
     if (isset($_GET["isbn"])) {
         $isbn = $_GET["isbn"];
     }
+    $booksList = dbconnection("spSelectUserSellBook(NULL, NULL,\"". $isbn ."\" ,NULL, NULL, NULL)");
     $book = dbconnection("spSelectSingleBook( \"". $isbn ."\")")[0];
-    $booksList = dbconnection("spSelectUserSellBook(NULL, NULL,\"". $isbn ."\" ,NULL, NULL, NULL)")[0];
 ?> 
 
 <!doctype html>
@@ -39,6 +39,32 @@
                             </strong> 
                         </p>
                         <button type="button" class="btn btn-warning" a href="postABook.php"> Sell Your Textbook </button>
+   						 <div class="row 1">
+							<div class="col-sm-auto mt-3">
+								<h2> Filters </h2>
+                                    <form method="get" action="/s" class="aok-inline-block a-spacing-none">
+                                        
+                                        <span class="a-dropdown-container"><label for="s-result-sort-select" class="a-native-dropdown"><b> Sort by:</b> </label><select name="<?php echo $booksList["price"];?>" autocomplete="on" id="s-result-sort-select" tabIndex="-1" class="a-native-dropdown">
+                                            
+                                                <option value="price-asc-rank">Price: Low to High</option>
+                                            
+                                        <option value="price-desc-rank">Price: High to Low</option>
+
+                                        <option value="date-desc-rank">Newest Arrivals</option>
+
+                                    </select><span tabIndex="-1" aria-label="Sort by: " class="a-button a-button-dropdown a-button-small"><span class="a-button-inner"><span class="a-button-text a-declarative" data-action="a-dropdown-button" role="button" tabIndex="0" aria-hidden="true"><span class="a-dropdown-label"></span><span class="a-dropdown-prompt"></span></span><i class="a-icon a-icon-dropdown"></i></span></span></span>
+                            </form>
+                            <form method="get" action="/s" class="aok-inline-block a-spacing-none">
+
+                                <span class="a-dropdown-container"><label for="s-result-sort-select" class="a-native-dropdown"><b>Condition: </b> </label><select name="<?php echo $booksList["bookCondition"];?>" autocomplete="on" id="s-result-sort-select" tabIndex="-1" class="a-native-dropdown">
+
+                                        <option value="checkbox"> Mint</option>
+                                        <option value="checkbox"> Good</option>
+                                        <option value="checkbox"> Fair</option>
+                                        <option value="checkbox"> Poor</option>
+                                        
+                                    </select><span tabIndex="-1" aria-label="Conditon: " class="a-button a-button-dropdown a-button-small"><span class="a-button-inner"><span class="a-button-text a-declarative" data-action="a-dropdown-button" role="button" tabIndex="0" aria-hidden="true"><span class="a-dropdown-label"></span><span class="a-dropdown-prompt"></span></span><i class="a-icon a-icon-dropdown"></i></span></span></span>
+                            </form>
                             <div class="row mb-3">
                                 <div class="col-sm-12 col-md-9 col-lg-6 col-xl-5">
                                     <div class="row">
@@ -59,6 +85,7 @@
         </div>
      </div>
     </div>
+
     </main>
 
     <?php include("resources/includes/footer.inc.php"); ?>
