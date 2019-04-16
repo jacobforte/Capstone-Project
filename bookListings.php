6e686986d1bfc9f947601ca2428d39bf08982a49
Time to upgrade? <vta@kent.edu>
2019-04-16 14:40:55 -0400

    Update bookListings.php

--- bookListings.php
+++ bookListings.php
@@ -39,32 +39,117 @@
                             </strong> 
                         </p>
                         <button type="button" class="btn btn-warning" a href="postABook.php"> Sell Your Textbook </button>
-   						 <div class="row 1">
-							<div class="col-sm-auto mt-3">
-								<h2> Filters </h2>
-                                    <form method="get" action="/s" class="aok-inline-block a-spacing-none">
-                                        
-                                        <span class="a-dropdown-container"><label for="s-result-sort-select" class="a-native-dropdown"><b> Sort by:</b> </label><select name="<?php echo $booksList["price"];?>" autocomplete="on" id="s-result-sort-select" tabIndex="-1" class="a-native-dropdown">
-                                            
-                                                <option value="price-asc-rank">Price: Low to High</option>
-                                            
-                                        <option value="price-desc-rank">Price: High to Low</option>
+                        <form action='<?php echo $_SERVER['UserSellBook']; ?>' method='post' name='form_filter' >
 
-                                        <option value="date-desc-rank">Newest Arrivals</option>
+<select name="value">
+    <option value="all">All</option>
+    <option value="price-asc-rank">Price: Low to High</option>     
+    <option value="price-desc-rank">Price: High to Low</option>
+    <option value="date-desc-rank">Newest Arrivals</option>
 
-                                    </select><span tabIndex="-1" aria-label="Sort by: " class="a-button a-button-dropdown a-button-small"><span class="a-button-inner"><span class="a-button-text a-declarative" data-action="a-dropdown-button" role="button" tabIndex="0" aria-hidden="true"><span class="a-dropdown-label"></span><span class="a-dropdown-prompt"></span></span><i class="a-icon a-icon-dropdown"></i></span></span></span>
-                            </form>
-                            <form method="get" action="/s" class="aok-inline-block a-spacing-none">
+                                    
+</select>
 
-                                <span class="a-dropdown-container"><label for="s-result-sort-select" class="a-native-dropdown"><b>Condition: </b> </label><select name="<?php echo $booksList["bookCondition"];?>" autocomplete="on" id="s-result-sort-select" tabIndex="-1" class="a-native-dropdown">
+<br />
 
-                                        <option value="checkbox"> Mint</option>
-                                        <option value="checkbox"> Good</option>
-                                        <option value="checkbox"> Fair</option>
-                                        <option value="checkbox"> Poor</option>
-                                        
-                                    </select><span tabIndex="-1" aria-label="Conditon: " class="a-button a-button-dropdown a-button-small"><span class="a-button-inner"><span class="a-button-text a-declarative" data-action="a-dropdown-button" role="button" tabIndex="0" aria-hidden="true"><span class="a-dropdown-label"></span><span class="a-dropdown-prompt"></span></span><i class="a-icon a-icon-dropdown"></i></span></span></span>
-                            </form>
+<input type='submit' value = 'Filter'>
+
+</form>
+<?php
+
+//connect to database, checking, etc
+
+if($_POST['value'] == 'price')) {
+// query to get all price 
+$query = "SELECT * FROM price WHERE name='price'";
+}
+elseif($_POST['value'] == 'date') {
+// query to get all date 
+$query = "SELECT * FROM date WHERE name='date'";
+}
+} else {
+// query to get all 
+$query = "SELECT * FROM price";
+}
+$sql = mysql_query($query);
+
+while ($row = mysql_fetch_array($query)){
+    $price = $row["price"];
+    $date = $row["date"];        
+?>
+                    <form action='<?php echo $_GET['UserSellBook']; ?>' method='post' name='form_filter' >
+
+                    <select name="checkbox">
+                        <option value="all">All</option>
+                        <option value="checkbox"> Mint</option>
+                        <option value="checkbox"> Good</option>
+                        <option value="checkbox"> Fair</option>
+                        <option value="checkbox"> Poor</option>
+                                                        
+                    </select>
+                    
+                    <br />
+                    
+                    <input type='submit' value = 'Filter'>
+
+                </form>
+                <?php
+
+                //connect to database, checking, etc
+
+                if($_POST['value'] == 'Mint') {
+                    // query to get all Mint 
+                    $query = "SELECT * FROM bookCondition WHERE name='Mint'";
+                }
+                elseif($_POST['value'] == 'Good') {
+                    // query to get all Good 
+                    $query = "SELECT * FROM bookCondition WHERE name='Good'";
+                }elseif($_POST['value'] == 'Fair') {
+                    // query to get all Fair 
+                    $query = "SELECT * FROM bookCondition WHERE name='Fair'";
+                }elseif($_POST['value'] == 'Poor') {
+                    // query to get all Poor 
+                    $query = "SELECT * FROM bookCondition WHERE name='Poor'";
+                } else {
+                    // query to get all condition
+                    $query = "SELECT * FROM ";
+                }
+                $sql = mysql_query($query);
+
+                while ($row = mysql_fetch_array($query)){
+                        $bookCondition = $row["bookCondition"];
+                ?>
+                <form action='<?php echo $_GET['UserSellBook']; ?>' method='post' name='form_filter' >
+
+                <select name="value">
+                    <option value="all">All</option>
+                    <option value="price-asc-rank">Price: Low to High</option>     
+                    <option value="price-desc-rank">Price: High to Low</option>
+
+                                                    
+                </select>
+
+                <br />
+
+                <input type='submit' value = 'Filter'>
+
+                </form>
+                <?php
+
+                //connect to database, checking, etc
+
+                if($_POST['value'] == 'price') {
+                // query to get price 
+                $query = "SELECT * FROM price WHERE name='price'";
+                } else {
+                // query to get all price
+                $query = "SELECT * FROM price";
+                }
+                $sql = mysql_query($query);
+
+                while ($row = mysql_fetch_array($query)){
+                    $price = $row["price"];       
+                ?>
                             <div class="row mb-3">
                                 <div class="col-sm-12 col-md-9 col-lg-6 col-xl-5">
                                     <div class="row">
