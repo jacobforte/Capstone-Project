c1d3224e45a09d366edaf1e7ac779245eabd59ce
zbrockway <zbrockwa@kent.edu>
2019-04-16 22:59:14 -0400

    Basic profile notifications markup

--- profileNotifications.php
+++ profileNotifications.php
@@ -1,3 +1,4 @@
+<?php include('resources/functions/account/account.notifications.list.function.php'); ?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -5,7 +6,7 @@
     </head>
     <body>
         <?php include("resources/includes/header.inc.php"); ?>
-
+        <?php include_once("resources/includes/check.php"); ?>
         <main>
             <div class="container-fluid">
                 <div class="row">
@@ -16,7 +17,29 @@
                             <a class="h5 mb-1 text-blue font-weight-bold flex-fill text-lg-right text-center nav-link" href="profileCourses.php">Courses</a>
                             <a class="h5 mb-1 text-blue font-weight-bold flex-fill text-lg-right text-center nav-link" href="profileListings.php">Listings</a>
                         </nav>
-                        
+                    </div>
+                    <div class="col-12 col-lg-10 pt-4 pb-4">
+                        <div class="row mb-3">
+                            <div class="col-12">
+                                <h4 class="font-weight-bold">Your Notifications</h4>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-12">
+                                <h5>Email me if...</h5>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-12">
+                                <?php outputUserNotifications($_SESSION['user']['email']); ?>
+                                <a href="#" class="btn btn-warning">Update</a>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-12">
+                                <p class="small">All emails will be sent to the email address found in <a href="profileAccount.php">Account</a></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ resources/functions/account/account.notifications.list.function.php
@@ -0,0 +1,18 @@
+<?php
+require("resources/functions/dbconnection.function.php");
+
+function outputUserNotifications($email) {
+    $notifications = dbconnection("spSelectNotifications");
+    $userNotifications = dbconnection("spSelectUserNotifications(\"" . $email . "\")");
+    
+    foreach ($notifications as $notification) {
+        echo '<div class="custom-control custom-checkbox mb-3">
+            <input type="checkbox" class="custom-control-input" id="' . $notification["type"] . '"';
+
+        if (in_array($notification, $userNotifications))
+            echo 'checked';
+
+            echo '><label class="custom-control-label" for="seatCapacity">' . $notification["description"]. '</label>
+          </div>';
+    }
+}
