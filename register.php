903a075f14572c5984ee371d1a7c978ea6867650
mjohn113 <mjohn113@kent.edu>
2019-04-23 16:19:14 -0400

    requires information to be filled out

--- register.php
+++ register.php
@@ -20,19 +20,19 @@
                         <form action="post.php" method="post">
 							<div class="form-group text-left">
 								<label for="email">Email address:</label>
-								<input type="email" class="form-control" name="email">
+								<input type="email" class="form-control" name="email" required>
 							</div>
 							<div class="form-group text-left">
 								<label for="email">Display Name:</label>
-								<input type="text" class="form-control" name="dnam">
+								<input type="text" class="form-control" name="dnam" required>
 							</div>
 							<div class="form-group text-left">
 								<label for="pwd">Password:</label>
-								<input type="password" class="form-control" name="pwd">
+								<input type="password" class="form-control" name="pwd" required>
 							</div>
 							<div class="form-group text-left">
 								<label for="pwd">Confirm Password:</label>
-								<input type="password" class="form-control" name="confirm">
+								<input type="password" class="form-control" name="confirm" required>
 							</div>
 								<button type="submit" class="btn btn-warning btn-block">Submit</button>
 								<br></br>
